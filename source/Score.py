589aa23a5018e2c18d2b19ea28eaed9d622159d1
Soneita <146932724+Soneita@users.noreply.github.com>
2023-11-26 16:56:56 +0100

    ScoreV2.py

--- source/Score.py
+++ source/Score.py
@@ -38,7 +38,7 @@ model = pickle.load(open('/Users/soneitaraherimalala/Desktop/P7/xgb_model.pkl', 
 
 # Chargez votre dataframe
 df_select = pd.read_csv('/Users/soneitaraherimalala/Desktop/P7/df_select.csv')
-
+print(df_select.columns)
 
 @app.route('/predict_score', methods=['POST'])
 def predict_score():
@@ -50,29 +50,26 @@ def predict_score():
        #Assurez-vous que les données reçues correspondent aux caractéristiques attendues par le modèle
         input_data = data.get('data')
         
-       
+        df_select[df_select['SK_ID_CURR']==input_data]
         print(input_data)
         if input_data is None:
-           return jsonify({'error': 'Missing data field'})
+            return jsonify({'error': 'Missing data field'})
 
-        # Transformez les données JSON en un DataFrame
-        client = df_select.loc[[input_data]]
-        # Assurez-vous que les colonnes du DataFrame sont dans le même ordre que celles du modèle
-        #client = df_select.loc[[input_data], columns=df_select.columns]
+# Sélectionnez les données du DataFrame correspondant à l'identifiant SK_ID_CURR spécifié
+        client = df_select[df_select['SK_ID_CURR'] == input_data]
 
-        print(client.shape)
-       
-        print(client.shape)
-        # Assurez-vous que le DataFrame a les mêmes caractéristiques que celui sur lequel le modèle a été formé
-        # Vous devrez peut-être effectuer un prétraitement sur les données en entrée pour les adapter au modèle si nécessaire.
-        
-        
+# Vérifiez si des données ont été trouvées pour l'identifiant donné
+        if client.empty:
+            return jsonify({'error': 'SK_ID_CURR not found'})
+
+# Utilisez les données sélectionnées pour la prédiction
         y_pred_classes = model.predict_proba(client)[:, 1]  # Obtenez la probabilité de la classe positive
 
-       # Convertissez le tableau NumPy en une liste Python avant de le renvoyer dans la réponse JSON
+# Convertissez le tableau NumPy en une liste Python avant de le renvoyer dans la réponse JSON
         y_pred_classes_list = y_pred_classes.tolist()
-        # Retournez les scores prédits sous forme de liste
+# Retournez les scores prédits sous forme de liste
         return jsonify({'prediction': y_pred_classes_list})
+
     except Exception as e:
         return jsonify({'error': str(e)})
 
